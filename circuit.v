e08f921024a167ea49c2b2757980f837cba89991
Pontus Thomsson <pontusthomsson@gmail.com>
2024-03-19 06:25:10 +0100

    Changed to verilog.

---
+++ circuit.v
@@ -0,0 +1,14 @@
+`default_nettype none
+
+module Circuit (
+    output logic b,
+    input logic clk,
+    input logic enable,
+    input logic a
+);
+  always_ff @(posedge clk)
+    if (enable) 
+        b <= a;
+    else 
+        b <= 0;
+endmodule
